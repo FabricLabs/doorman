151fa84678ef88ebdff0d6e7e86f13e0f06b328a
Eric Martindale <eric@ericmartindale.com>
2017-10-03 09:56:17 -0700

    Starting work with electron

--- doorman.js
+++ doorman.js
@@ -2,7 +2,19 @@ var config = require('./config');
 
 var Maki = require('maki');
 var doorman = new Maki(config);
+var electron = require('electron');
+var BrowserWindow = electron.BrowserWindow;
+
+var path = require('path');
+var url = require('url');
+
+doorman.define('Invitation', {
+  email: { type: String }
+});
 
 doorman.use(require('maki-client-level'));
 
-doorman.start();
+doorman.start(function() {
+  // TODO: allow build to be called without server
+  doorman.build();
+});
