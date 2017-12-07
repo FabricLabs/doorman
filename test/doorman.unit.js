d95155d0c5ea77fb2e4c0fdc47420c893b2aaef6
Eric Martindale <eric@ericmartindale.com>
2017-12-06 16:43:02 -0800

    Add a test for ready event

--- test/doorman.unit.js
+++ test/doorman.unit.js
@@ -7,4 +7,12 @@ describe('Doorman', function () {
   it('should expose a constructor', function () {
     assert(typeof Doorman, 'function');
   });
+  
+  it('can initialize a connection', function (done) {
+    var doorman = new Doorman();
+    
+    doorman.on('ready', done);
+    
+    doorman.start();
+  });
 });
