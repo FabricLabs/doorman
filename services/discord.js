532ee8f42f12e29a5ae730d0ff55b0ca1ca92258
Nate Richardson <hello@naterichardson.com>
2018-04-27 12:09:48 -0400

    tabs > spaces <3

--- services/discord.js
+++ services/discord.js
@@ -5,32 +5,32 @@ const DiscordJS = require('discord.js');
 const Service = require('../lib/service');
 
 function Discord(config) {
-	this.config = config || {};
-	this.connection = null;
-	this.map = {};
+  this.config = config || {};
+  this.connection = null;
+  this.map = {};
 }
 
 util.inherits(Discord, Service);
 
 Discord.prototype.connect = function initialize() {
-	if (this.config.bot_token) {
-		this.connection = new DiscordJS.Client();
-		this.connection.login(this.config.bot_token);
-		this.connection.on('ready', this.ready.bind(this));
-		this.connection.on('message', this.handler.bind(this));
-	}
+  if (this.config.bot_token) {
+    this.connection = new DiscordJS.Client();
+    this.connection.login(this.config.bot_token);
+    this.connection.on('ready', this.ready.bind(this));
+    this.connection.on('message', this.handler.bind(this));
+  }
 };
 
 Discord.prototype.handler = function route(message) {
-	this.emit('message', {
-		actor: message.author.id,
-		target: message.channel.id,
-		object: message.content
-	});
+  this.emit('message', {
+    actor: message.author.id,
+    target: message.channel.id,
+    object: message.content
+  });
 };
 
 Discord.prototype.send = function send(channel, message) {
-	this.connection.channels.get(channel).send(message);
+  this.connection.channels.get(channel).send(message);
 };
 
 module.exports = Discord;
